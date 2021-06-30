45350e3a60d9b4495cf3b484fcf1f0c81c8d3f03
Kay Kim <kimjh@bawi.org>
2021-06-30 10:50:21 +0900

    initialize repo

---
+++ src/App.tsx
@@ -0,0 +1,53 @@
+import React, { useState } from 'react';
+
+import logo from './logo.svg';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="text-center">
+      <header className="flex flex-col items-center justify-center min-h-screen text-white bg-[#282c34] text-3xl">
+        <img
+          src={logo}
+          className="h-[40vmin] pointer-events-none animate-spin-slow motion-safe:animate-none"
+          alt="logo"
+        />
+        <p className="text-red-500">Hello Vite + React!</p>
+        <p>
+          <button
+            className="text-3xl hover:underline"
+            type="button"
+            onClick={() => setCount((count) => count + 1)}
+          >
+            count is: {count}
+          </button>
+        </p>
+        <p>
+          Edit <code>App.tsx</code> and save to test HMR updates. (fixed)
+        </p>
+        <p>
+          <a
+            className="text-[#61dafb]"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+          {' | '}
+          <a
+            className="text-[#61dafb]"
+            href="https://vitejs.dev/guide/features.html"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Vite Docs
+          </a>
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  mode: 'jit',
+  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      animation: {
+        'spin-slow': 'spin 20s linear infinite',
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
